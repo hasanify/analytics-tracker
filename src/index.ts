a30077e84ecefccbf554c1bf838fd31e3a2241c0
hasanify <hasanali.abps@gmail.com>
2024-08-16 18:34:07 +0530

    print version on start

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,17 @@ import "module-alias/register";
 
 import env from "@/data/env";
 import createServer from "@/lib/server";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 const app = createServer();
 
 app.listen(env.PORT, () => {
+  const packageJsonPath = join(__dirname, "..", "package.json");
+  const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
+  console.log("--------------------------------------------");
+  console.log(`Started v${packageJson.version}! 🚀`);
+  console.log("--------------------------------------------");
+
   console.log(`Server Running on :${env.PORT}`);
 });
